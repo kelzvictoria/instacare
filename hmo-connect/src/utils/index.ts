2cd2ff81bdb1de87a0b2d1d85a39c926446b204c
Victoria Kazeem <victoria.kazeem@pmglobaltechnology.com>
2021-03-02 10:40:19 +0100

    Deleted hmo-connect folder from the project folder

--- hmo-connect/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import numeral from "numeral";
-
-export const NAIRA_SIGN = "₦";
-
-export const formatAsCurrency = (value: number) => `${NAIRA_SIGN}${numeral(value).format("0,0")}`;
-
-export const PAYSTACK_PUBLIC_KEY="pk_test_0c1c04d62b1746b3d59210fdebcded80735e9356";
